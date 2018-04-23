e47d2758681ae3f12702d72775599e4b4755caa1
CathodeRayBlues <36201999+CathodeRayBlues@users.noreply.github.com>
2018-04-23 09:31:27 -0700

    Added Placeholder UI
    the UI images and elements were stolen right from SLUMO for a quick and easy HUD for presenting the unfinished game for mid-quarter. We should change the images to be new later.

--- Q4 project/Assets/SCRIPTS/youreWinner.cs
+++ Q4 project/Assets/SCRIPTS/youreWinner.cs
@@ -9,13 +9,15 @@ public class youreWinner : MonoBehaviour {
     public GameObject p2;
     public GameObject p3;
     public GameObject p4;
+    //LAVA
+    public GameObject lava;
     //UI ELEMENTS
     public GameObject wintext1;
     public GameObject wintext2;
     public GameObject wintext3;
     public GameObject wintext4;
 
-    //player out elements
+    //player out elements GONNA REPLACE THESE WITH ANNOUNCER SOUNDS MAYBE
     public GameObject p1out;
     public GameObject p2out;
     public GameObject p3out;
@@ -41,11 +43,11 @@ public class youreWinner : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if (p1.transform.position.y < 626)
+        if (p1.transform.position.y < lava.transform.position.y)
         {
-            if (p2.transform.position.y < 626)
+            if (p2.transform.position.y < lava.transform.position.y)
             {
-                if (p3.transform.position.y < 626)
+                if (p3.transform.position.y < lava.transform.position.y)
                 {
                     //PLAYER 4 WINS
                     Debug.Log("player 4 wins");
@@ -61,7 +63,7 @@ public class youreWinner : MonoBehaviour {
 
                 }
 
-                else if (p4.transform.position.y < 626)
+                else if (p4.transform.position.y < lava.transform.position.y)
 
                 {
                     //PLAYER 3 WINS
@@ -78,7 +80,7 @@ public class youreWinner : MonoBehaviour {
                 }
             }
 
-            else if (p3.transform.position.y < 626 && p4.transform.position.y < 626)
+            else if (p3.transform.position.y < lava.transform.position.y && p4.transform.position.y < lava.transform.position.y)
             {
                 //PLAYER 2 WINS
                 Debug.Log("player 2 wins");
@@ -95,7 +97,7 @@ public class youreWinner : MonoBehaviour {
 
         }
 
-        else if (p3.transform.position.y < 626 && p4.transform.position.y < 628 && p2.transform.position.y < 626)
+        else if (p3.transform.position.y < lava.transform.position.y && p4.transform.position.y < 628 && p2.transform.position.y < lava.transform.position.y)
 
         {
             //PLAYER 1 WINS
@@ -121,27 +123,31 @@ public class youreWinner : MonoBehaviour {
         //PLAYER IS OUT functions
 
         //player one out!
-        if (p1.transform.position.y < 628)
+        if (p1.transform.position.y < lava.transform.position.y)
         {
-            p1out.gameObject.SetActive(true);
+            p1.GetComponent<Rigidbody>().detectCollisions = false;
+            //PLAYER ONE OUT SOUND
         }
 
         //player two out!
-        if (p2.transform.position.y < 628)
+        if (p2.transform.position.y < lava.transform.position.y)
         {
-            p2out.gameObject.SetActive(true);
+            p2.GetComponent<Rigidbody>().detectCollisions = false;
+            //PLAYER 2 OUT SOUND
         }
 
         //player three out!
-        if (p3.transform.position.y < 628)
+        if (p3.transform.position.y < lava.transform.position.y)
         {
-            p3out.gameObject.SetActive(true);
+            p3.GetComponent<Rigidbody>().detectCollisions = false;
+            //PLAYER 3 OUT SOUND
         }
 
         //player four out!
-        if (p4.transform.position.y < 628)
+        if (p4.transform.position.y < lava.transform.position.y)
         {
-            p4out.gameObject.SetActive(true);
+            p4.GetComponent<Rigidbody>().detectCollisions = false;
+            //PLAYER 4 OUT SOUND
         }
     }
 }
